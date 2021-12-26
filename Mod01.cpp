93447c579904002a9d71d80576b6a5b29433b96e
yeargin2021 <88904457+yeargin2021@users.noreply.github.com>
2021-12-26 13:26:42 -0500

    commit

---
+++ Mod01.cpp
@@ -0,0 +1,16 @@
+//
+//  Mod01.cpp
+//  Searching_for_linker_error_c++
+//
+//  Created by TOMMY YEARGIN on 12/26/21.
+//
+
+#include "Mod01.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "hi\n";
+    return 0;
+}
---
+++ Mod01.hpp
@@ -0,0 +1,13 @@
+//
+//  Mod01.hpp
+//  Searching_for_linker_error_c++
+//
+//  Created by TOMMY YEARGIN on 12/26/21.
+//
+
+#ifndef Mod01_hpp
+#define Mod01_hpp
+
+#include <stdio.h>
+
+#endif /* Mod01_hpp */
